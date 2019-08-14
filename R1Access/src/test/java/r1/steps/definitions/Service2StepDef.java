c28f5f5f066684bbf7147c4a35ff2dcaaa3e13e0
IN302616 <IN302616@NDR1-D85QF622A.india.accretivehealth.local>
2019-08-14 14:28:19 +0530

    checkin

--- R1Access/src/test/java/r1/steps/definitions/Service2StepDef.java
+++ R1Access/src/test/java/r1/steps/definitions/Service2StepDef.java
@@ -10,16 +10,15 @@ import r1.commons.BasePage;
 import r1.commons.R1AccessCommonMethods;
 import r1.pages.Service2Page;
 
-public class Service2StepDef extends BasePage{
-	
+public class Service2StepDef extends BasePage {
+
 	R1AccessCommonMethods r1AccessCommonMethod;
 	Service2Page service2;
-				
+
 	@When("^user clicks on any Unassigned Account$")
 	public void user_clicks_on_any_Unassigned_Account() {
 		r1AccessCommonMethod.clickOnUnassignedAccount();
 	}
-
 
 	@When("^user clicked on \"([^\"]*)\" tab$")
 	public void user_clicked_on_tab(String Text) {
@@ -35,7 +34,7 @@ public class Service2StepDef extends BasePage{
 	public void user_clicked_on_checkout_button() {
 		r1AccessCommonMethod.clickOnCheckOut();
 	}
-	
+
 	@When("^user clicked on Admitting if exists$")
 	public void user_clicked_on_Admitting_if_exists() {
 		service2.clickOnAdmiting();
@@ -43,7 +42,7 @@ public class Service2StepDef extends BasePage{
 
 	@Then("^verify the fields after the checkout$")
 	public void verify_the_fields_after_the_checkout() {
-		r1AccessCommonMethod.verifyActionStatusAfterCheckout();  
+		r1AccessCommonMethod.verifyActionStatusAfterCheckout();
 	}
 
 	@When("^user enter services like \"([^\"]*)\" in search field$")
@@ -53,7 +52,7 @@ public class Service2StepDef extends BasePage{
 	}
 
 	@Then("^searched result should be shown in service pane$")
-	public void searched_result_should_be_shown_in_service_pane() {	    
+	public void searched_result_should_be_shown_in_service_pane() {
 		service2.verifyServiceSearchResults();
 	}
 
@@ -74,7 +73,7 @@ public class Service2StepDef extends BasePage{
 
 	@When("^user delete added services$")
 	public void user_delete_added_services() {
-		service2.deleteServices();		
+		service2.deleteServices();
 	}
 
 	@Then("^service search result grid will not be displayed$")
@@ -92,38 +91,27 @@ public class Service2StepDef extends BasePage{
 	public void single_multiple_radio_buttons_of_pricing_will_not_be_displayed() {
 		service2.verifySingleMultipleRadioBtnNotExists();
 	}
-	
+
 	@When("^user add ICD Codes$")
-	public void user_add_ICD_Codes() {	    
+	public void user_add_ICD_Codes() {
 		service2.addICDDiagnosisCodes();
 	}
 
+	@Then("^verify the status of the tab is completed$")
+	public void verify_the_status_of_the_tab_is_completed() {
+		service2.verifyTabColorAndCompleteStatus();
 
-   @Then("^verify the status of the tab is completed$")
-   public void verify_the_status_of_the_tab_is_completed() {  
-	   service2.verifyTabColorAndCompleteStatus();
-	   
-   }
+	}
 
+	@Then("^verify that up down arrow key is moving of ICD codes$")
+	public void verify_that_up_down_arrow_key_is_moving_of_ICD_codes() {
 
-  @Then("^verify that up down arrow key is moving of ICD codes$")
-  public void verify_that_up_down_arrow_key_is_moving_of_ICD_codes() {
-   
-	  
-	  
-	  
-	  
-	  
-	  
-  }
+	}
 
-@When("^user clicked on \"([^\"]*)\" btn$")
-public void user_clicked_on_btn(String arg1) {
-	
-	service2.clickOnExceptionContinueButton("Continue");
+	@When("^user clicked on \"([^\"]*)\" btn$")
+	public void user_clicked_on_btn(String arg1) {
+
+		service2.clickOnExceptionContinueButton("Continue");
+	}
+
 }
-	
-	
-}
-
-
