dcef3f2fc7b0d51b130ca523f46bf4c98b743ec5
301331 <301331@NDR1-DHXPF622B.india.accretivehealth.local>
2019-08-19 17:29:26 +0530

    DataAccess File checked IN

---
+++ R1Access/src/test/java/r1/commons/databaseconnection/DataAccess.java
@@ -0,0 +1,14 @@
+package r1.commons.databaseconnection;
+import java.io.IOException;
+import java.sql.SQLException;
+import r1.commons.BasePage;
+import r1.commons.utilities.CommonMethods;
+
+public class DataAccess extends BasePage{	
+	public static String getEncounterId(String column,String Query) throws IOException, ClassNotFoundException, SQLException{
+		DatabaseConn.getServerDBName(CommonMethods.LoadProperties("webdriver.base.url"), CommonMethods.LoadProperties("facility"));
+		DatabaseConn.serverConn(DatabaseConn.serverName,DatabaseConn.databaseName, Query);
+	    DatabaseConn.resultSet.next();
+		return DatabaseConn.resultSet.getString(column);
+		}	
+}
