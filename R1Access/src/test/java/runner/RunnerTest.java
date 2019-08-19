7cf8e5680a0a286df98c1bc2d83e9a1aed64f194
Avinash Kumar <akumar188@r1rcm.com>
2019-08-19 08:06:15 +0000

    RunnerTest.java edited online with Bitbucket

--- R1Access/src/test/java/runner/RunnerTest.java
+++ R1Access/src/test/java/runner/RunnerTest.java
@@ -8,7 +8,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = { "src/test/resources/features/R1Access" },
-tags = { "@FCC"}, 
 glue = "r1.steps.definitions")
 
 public class RunnerTest {
