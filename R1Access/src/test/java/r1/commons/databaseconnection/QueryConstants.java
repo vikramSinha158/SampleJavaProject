3c8661f09e4b133e0b6b299bb132e60ff2890238
ranand1@r1rcm.com <ranand1@r1rcm.com>
2019-08-23 12:54:31 +0000

    QueryConstants.java edited online with Bitbucket

--- R1Access/src/test/java/r1/commons/databaseconnection/QueryConstants.java
+++ R1Access/src/test/java/r1/commons/databaseconnection/QueryConstants.java
@@ -49,5 +49,4 @@ public class QueryConstants {
 				"(select ID, EncounterID, PatientType from Registrations R (nolock) " + 
 				"where IsDischarged=0) R Join services s (nolock) on R.ID <> s.RegistrationID";
 	}
-
 }
