ae02b2468a79bcc06bc004b178cdd45504fe1ebc
IN303082 <IN303082@ACH-D7120-001.india.accretivehealth.local>
2019-08-23 17:10:56 +0530

    PFA DataAccess has been checked In

--- R1Access/src/test/java/r1/commons/databaseconnection/QueryConstantPFA.java
+++ R1Access/src/test/java/r1/commons/databaseconnection/QueryConstantPFA.java
@@ -3,7 +3,7 @@ package r1.commons.databaseconnection;
 import r1.commons.BasePage;
 
 public class QueryConstantPFA extends BasePage{
-	
+
 	public String PFA_419028_SQL10()
 	{
 		return "select top 1 EncounterID from registrations where sex='M'";
@@ -14,27 +14,23 @@ public class QueryConstantPFA extends BasePage{
 		return "\r\n" + 
 				"select top 1 v.encounterid,r.sex, (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) as age from vwRegistrationProcessUnscreened v inner join registrations r on  v.encounterid=r.encounterid\r\n" + 
 				"				where r.sex='F' and (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) >'54'";
-		
+
 	}
-	
+
 	public String PFA_419030_SQL12()
 	{
 		return "select v.encounterid,r.sex, (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) as age from vwRegistrationProcessUnscreened v inner join registrations r on  v.encounterid=r.encounterid\r\n" + 
 				"where r.sex='F' and (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) <'54'";
 	}
-	
+
 	public static String queryPayorCode = "select * from payorplans where payorplanname like '%commercial%'";
-	
+
 	public static String queryEncounterdID = "select top 1 s.state,r.encounterid from vwRegistrationProcessUnscreened r  inner join accretive..state s on R.state=s.statecode";
-	
+
 	public static String queryAgeLessThanEighteenEncounterID = "SELECT TOP 1 r.ID, EncounterID FROM Registrations r (nolock) Left join recordcheckout rco (nolock) on rco.RecordKey = r.ID Where rco.ID is NULL and r.DOB IS NOT NULL and (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) < 18 ORDER BY 1 desc";
-	
+
 	public static String queryAgeGreaterThanEighteenEncounterID ="SELECT TOP 1 r.ID, EncounterID FROM Registrations r (nolock) Left join recordcheckout rco (nolock) on rco.RecordKey = r.ID Where rco.ID is NULL and r.DOB IS NOT NULL and (DATEDIFF(DAY, r.DOB, r.AdmitDate) / 365) >= 18 ORDER BY 1 desc"; 
-	
-	
-	
-	
-	
-	
+
+
 
 }
