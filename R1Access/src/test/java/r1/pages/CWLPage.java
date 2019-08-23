69b941ca4839f50d947bbaa9e9f919246ec69239
IN303321 <IN303321@ACH-D7121-002.india.accretivehealth.local>
2019-08-23 16:41:57 +0530

    " "

--- R1Access/src/test/java/r1/pages/CWLPage.java
+++ R1Access/src/test/java/r1/pages/CWLPage.java
@@ -28,353 +28,353 @@ public class CWLPage extends BasePage {
 	R1AccessCommonMethods common;
 	QueryConstantCwl cwlQuery;
 	LocalDateTime myDateObj=LocalDateTime.now();
-	
+
 	public String followUpWorklistRow = "//*[contains(@id,'grdWorklist')]/tbody/tr";
-  	public String followUpWorklistHeader = "//*[contains(@id,'grdWorklist')]/tbody/tr[1]/td";
-  	
-  	@FindBy(xpath ="//*[contains(@id,'grdWorklist')]/tbody/tr")
+	public String followUpWorklistHeader = "//*[contains(@id,'grdWorklist')]/tbody/tr[1]/td";
+
+	@FindBy(xpath ="//*[contains(@id,'grdWorklist')]/tbody/tr")
 	private List<WebElementFacade> AccountRecords;
-  	
+
 	@FindBy(xpath = "//td[contains(text(),'Patient Visit and Address')]")
 	private WebElementFacade verify_patient_tab;
-	
+
 	@FindBy(linkText = "Settings")
 	private WebElementFacade Settings;
-	
+
 	@FindBy(xpath = "//div[@id='sidebar']/div[1]/span[2]/span/div/span[2]")
 	private List<WebElementFacade> payor_config_submenu;
-	
+
 	@FindBy(xpath = "//*[@id='dnn_ctr1220_ConversionFundingSources_dgConversionFundingSource']/tbody/tr")
 	private List<WebElementFacade> conversionPlanList;
-	
+
 	@FindBy(xpath = "//div[@class='side_nav']//a /following-sibling::span[@class='txt']")
 	private List<WebElementFacade> Setting_submenu;	
-	
+
 	@FindBy(xpath = "//span[@igtxt='1']")
 	private List<WebElementFacade> conversionFollowList;
-	
+
 	@FindBy(xpath="//span[text()='I/S at Risk']")
 	private WebElementFacade ISRisk;
-	
+
 	@FindBy(xpath="//span[text()='E/O at Risk']")
 	private WebElementFacade EORisk;
-	
+
 	@FindBy(xpath="//span[text()='Future Follow Up']")
 	private WebElementFacade FutureFollowUp;
-	
+
 	@FindBy(xpath="//span[text()='Zero Balance']")
 	private WebElementFacade zeroBalance;
-	
+
 	String[] conversionList = {"Conversion Followup","Pending","","","","","","","","On-Deck","I/S at Risk","E/O at Risk","Future Follow Up","Zero Balance","Supervisor Worklist","Care Coverage","","","BSO"};
 	ArrayList<String> namesList = new ArrayList<String>();
-	
+
 	@FindBy(xpath = "//table[@class='worklistTable']/tbody/tr")
 	private List<WebElementFacade> conversionFollowtableRows;
-	
+
 	@FindBy(xpath="//a[contains(@id,'_grid_lnkClear')]")
 	private List<WebElementFacade>  showAll;
-	
+
 
 	@FindBy(xpath="//table[@class='worklistTable']/tbody/tr[")
 	private WebElementFacade beforeXpath;
-	
+
 	@FindBy(xpath="]/td[5]/a")
 	private WebElementFacade afterXpath;
-	
-	
+
+
 	public void click_setting()
 	{
 		Settings.click();
-		
+
 	}
-	
+
 	public void submenuList()
 	{
 		int size=Setting_submenu.size();
-		
-		
+
+
 		for(int i=0;i<size;i++)
 		{
 			String submenu=Setting_submenu.get(i).getText();
 			System.out.println(submenu);
 		}
-		
+
 	}
-		public void select_submenu( String menu)
+	public void select_submenu( String menu)
+	{
+		common.clickSubSideR1HubModulesMenuLink(menu);
+
+	}
+	public void payorcon_submenu( String menu)
+	{
+		common.clickSubSideR1HubModulesMenuLink(menu);
+	}
+
+
+
+	public void settingSubmenuList()
+	{
+		for(int i=0;i<Setting_submenu.size();i++)
 		{
-			common.clickSubSideR1HubModulesMenuLink(menu);
-			
+			String submenu=Setting_submenu.get(i).getText();
+			System.out.println(submenu);
 		}
-			public void payorcon_submenu( String menu)
+	}
+
+	public void payorConfiglist()
+	{
+		for(int i=0;i<Setting_submenu.size();i++)
+		{
+			String submenu=Setting_submenu.get(i).getText();
+			System.out.println(submenu);
+		}
+	}
+
+	public void verifyCPMSize()
+	{
+		int size=conversionPlanList.size();
+		System.out.println(size);
+		Assert.assertFalse(size==0);
+
+	}
+	/*-------------------------------------------------------------Test Case 391787:CWL_Verify the filter folders in the worklist-------------------------------------------------------------*/
+
+
+	// click on patient access
+	public void clickOnpatientAccess()
+	{
+		common.clickFooterR1AccesModulesTab("PATIENT ACCESS");
+	}
+	public void clickConversionfollowup()
+	{
+		common.clickSubSideR1HubModulesMenuLink("Conversion Followup");
+	}
+
+	// Verify Conversion Follow up filter folder lists
+
+	public void filterList()
+	{
+
+		for(int i=0;i<conversionFollowList.size();i++) {
+			String optionValue=conversionFollowList.get(i).getText();
+
+			if(optionValue.equals(""))
 			{
-				common.clickSubSideR1HubModulesMenuLink(menu);
+				continue;
 			}
-			
-			
-			
-			public void settingSubmenuList()
+			else if(optionValue.equals(conversionList[i]))
 			{
-				for(int i=0;i<Setting_submenu.size();i++)
+				Assert.assertTrue("passed on: " + optionValue, true);
+			}
+			else
+			{
+
+				Assert.assertTrue("failed on: " + optionValue, false);
+			}}
+	}
+
+
+	/*-------------------------------------------------------------Test Case 391788:CWL_Verify records in "I/S at Risk" filter folder-------------------------------------------------------------*/
+
+
+	public void clickOnIsRisk()
+	{
+		clickShowAll();
+		ISRisk.click();
+	}
+
+
+	@SuppressWarnings("deprecation")
+
+	// verify PT value on Conversion Followup > I/S at Risk 
+
+	public void conversiontable(String val1, String val2)
+	{
+		ArrayList<String> ptCol= common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"PT");
+		if(AccountRecords.size()>1) {
+			for(int k=0;k<ptCol.size();k++)
+			{
+
+				String i=ptCol.get(k);
+				if(i.contains(val1))
 				{
-					String submenu=Setting_submenu.get(i).getText();
-					System.out.println(submenu);
-			}
-			}
-			
-			public void payorConfiglist()
-			{
-				for(int i=0;i<Setting_submenu.size();i++)
+					Assert.assertTrue(i+" is matching with"+" "+val1, true);
+
+
+				}else if(i.contains(val2))
 				{
-					String submenu=Setting_submenu.get(i).getText();
-					System.out.println(submenu);
+					Assert.assertTrue(i+" is matching with"+" "+val2, true);
+				}else
+				{
+					System.out.println("PT value is coming"+ i);
+					Assert.assertTrue(false);
+				}
+			}}else
+			{
+				Assert.assertTrue("No Accounts records are present", false);
 			}
 	}
-			
-			public void verifyCPMSize()
+
+	// verify NFU date on Conversion Followup > I/S at Risk 
+	public void verifyNfuDt() 
+	{
+		ArrayList<String> NFUdt= common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"NFU Dt");
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM/dd/yyyy"); 
+		String currentDate = myDateObj.format(myFormatObj); 
+		System.out.println("current date is: " + currentDate);
+		if(AccountRecords.size()>1) {
+			for(String nfudate:NFUdt)
 			{
-				int size=conversionPlanList.size();
-				System.out.println(size);
-				Assert.assertFalse(size==0);
-				
-			}
-	/*-------------------------------------------------------------Test Case 391787:CWL_Verify the filter folders in the worklist-------------------------------------------------------------*/
-			
-			
-			// click on patient access
-			public void clickOnpatientAccess()
-			{
-				common.clickFooterR1AccesModulesTab("PATIENT ACCESS");
-			}
-			public void clickConversionfollowup()
-			{
-				common.clickSubSideR1HubModulesMenuLink("Conversion Followup");
-			}
-			
-			// Verify Conversion Follow up filter folder lists
-			
-			public void filterList()
+				if(currentDate.compareTo(nfudate)<=0)
+				{
+					Assert.assertTrue("nfudate is not correct "+currentDate.compareTo(nfudate),false);
+
+				}
+				else if(nfudate.isEmpty())
+				{
+
+					Assert.assertTrue("nfudate is empty", true);
+
+				}else
+				{
+					Assert.assertTrue("current: "+currentDate+" nfudate: "+nfudate, true);
+
+				}
+
+
+			} 
 			{
 
-				for(int i=0;i<conversionFollowList.size();i++) {
-					String optionValue=conversionFollowList.get(i).getText();
-					
-					if(optionValue.equals(""))
-					{
-						continue;
-					}
-					else if(optionValue.equals(conversionList[i]))
-					{
-						Assert.assertTrue("passed on: " + optionValue, true);
-					}
-					else
-					{
-						
-						Assert.assertTrue("failed on: " + optionValue, false);
-					}}
-				}
-			
-			
-	/*-------------------------------------------------------------Test Case 391788:CWL_Verify records in "I/S at Risk" filter folder-------------------------------------------------------------*/
-	
-			
-			public void clickOnIsRisk()
+			}
+		}else
+		{
+			Assert.assertTrue("No Accounts records are present", false);
+		}
+	}
+
+
+	// Verify total balance on Conversion Followup > I/S at Risk
+
+	public void verifyTotalBal() {
+		ArrayList<String> openBal=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"Total Open Bal");
+		if(AccountRecords.size()>1) {
+			for(String TotalopenBal:openBal)
 			{
-				clickShowAll();
-				ISRisk.click();
+
+				double d = Double.parseDouble(TotalopenBal);
+				if(d==0.00 || d==0)
+				{
+
+					Assert.assertTrue("total balance is coming "+d,false);
+				}else
+				{
+					Assert.assertTrue("total balance is coming "+d,true);
+
+
+
+				}
 			}
 
-			
-			@SuppressWarnings("deprecation")
-			
-			// verify PT value on Conversion Followup > I/S at Risk 
-			
-			public void conversiontable(String val1, String val2)
+		}else {
+			Assert.assertTrue("No Accounts records are present", false);
+		}
+	}
+
+	// Verify total zero balance on Conversion Followup
+	public void verifyTotalzeroBal() {
+		ArrayList<String> openBal=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"Total Open Bal");
+		if(AccountRecords.size()>1) {
+			for(String TotalopenBal:openBal)
 			{
-			  ArrayList<String> ptCol= common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"PT");
-			 if(AccountRecords.size()>1) {
-			  for(int k=0;k<ptCol.size();k++)
-				 {
-					 
-					 String i=ptCol.get(k);
-					 if(i.contains(val1))
-					 {
-						 Assert.assertTrue(i+" is matching with"+" "+val1, true);
-						
-						 						
-					 }else if(i.contains(val2))
-					 {
-						 Assert.assertTrue(i+" is matching with"+" "+val2, true);
-					 }else
-					 {
-						 System.out.println("PT value is coming"+ i);
-						 Assert.assertTrue(false);
-					 }
-				 }}else
-				    {
-				    	Assert.assertTrue("No Accounts records are present", false);
-				    }
+
+				double d = Double.parseDouble(TotalopenBal);
+				if(d==0.00 || d==0)
+				{
+					Assert.assertTrue("Total balnce is coming "+d+"on zero balnce",true);
+
+				}else
+				{
+					Assert.assertTrue("Total balance is coming "+d,false);
+				}
 			}
-			
-			// verify NFU date on Conversion Followup > I/S at Risk 
-				public void verifyNfuDt() 
+
+		}else {
+			Assert.assertTrue("No Accounts records are present", false);
+		}
+	}
+
+
+
+
+	// Verify LA values on 	Conversion Followup > I/S at Risk
+	public void verifyLA() {
+		ArrayList<String> LA=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"LA");
+		if(AccountRecords.size()>1)
+		{
+			for(String LAvalue:LA)
+			{
+				if(LAvalue.contains("Referred to Care Coverage"))
 				{
-					ArrayList<String> NFUdt= common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"NFU Dt");
-					DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM/dd/yyyy"); 
-				    String currentDate = myDateObj.format(myFormatObj); 
-				    System.out.println("current date is: " + currentDate);
-				    if(AccountRecords.size()>1) {
-				  	for(String nfudate:NFUdt)
-					{
-						if(currentDate.compareTo(nfudate)<=0)
-								{
-							 Assert.assertTrue("nfudate is not correct "+currentDate.compareTo(nfudate),false);
-								
-								}
-						else if(nfudate.isEmpty())
-						{
-				
-							Assert.assertTrue("nfudate is empty", true);
-							 
-						}else
-						{
-							Assert.assertTrue("current: "+currentDate+" nfudate: "+nfudate, true);
-							
-						}
-						
-						
-					} 
-					{
-						
-					}
-				    }else
-				    {
-				    	Assert.assertTrue("No Accounts records are present", false);
-				    }
+					Assert.assertTrue("LA values is "+LAvalue,false);
 				}
-				
-				
-				// Verify total balance on Conversion Followup > I/S at Risk
-				
-				public void verifyTotalBal() {
-				ArrayList<String> openBal=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"Total Open Bal");
-				if(AccountRecords.size()>1) {
-				for(String TotalopenBal:openBal)
+				else if(LAvalue.contains("Referred to Supervisor"))
 				{
-					
-					double d = Double.parseDouble(TotalopenBal);
-					if(d==0.00 || d==0)
-					{
-						
-						Assert.assertTrue("total balance is coming "+d,false);
-					}else
-					{
-						Assert.assertTrue("total balance is coming "+d,true);
-						
-						
-						
-					}
+					Assert.assertTrue("LA values is "+LAvalue,false);
+
+				}else {
+					Assert.assertTrue("LA value is "+LAvalue,true);
+
 				}
-				
-			}else {
+			}}else
+			{
 				Assert.assertTrue("No Accounts records are present", false);
 			}
-			}
-				
-		// Verify total zero balance on Conversion Followup
-				public void verifyTotalzeroBal() {
-				ArrayList<String> openBal=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"Total Open Bal");
-				if(AccountRecords.size()>1) {
-				for(String TotalopenBal:openBal)
-				{
-					
-					double d = Double.parseDouble(TotalopenBal);
-					if(d==0.00 || d==0)
-					{
-						 Assert.assertTrue("Total balnce is coming "+d+"on zero balnce",true);
-						 
-					}else
-					{
-						Assert.assertTrue("Total balance is coming "+d,false);
-											}
-				}
-				
-			}else {
-				Assert.assertTrue("No Accounts records are present", false);
-			}
-			}
-			
-				
-				
-				
-			// Verify LA values on 	Conversion Followup > I/S at Risk
-				public void verifyLA() {
-					ArrayList<String> LA=  common.getTableColValue(followUpWorklistRow,followUpWorklistHeader,"LA");
-					if(AccountRecords.size()>1)
-					{
-					for(String LAvalue:LA)
-					{
-					  if(LAvalue.contains("Referred to Care Coverage"))
-						{
-							 Assert.assertTrue("LA values is "+LAvalue,false);
-						}
-					  else if(LAvalue.contains("Referred to Supervisor"))
-						{
-							Assert.assertTrue("LA values is "+LAvalue,false);
-							
-						}else {
-							Assert.assertTrue("LA value is "+LAvalue,true);
-							
-						}
-					}}else
-					{
-						Assert.assertTrue("No Accounts records are present", false);
-					}
-					
-				}
-				
-				public void clickShowAll()
-				{
-					int k=showAll.size();
-					if (k > 0) {
-						showAll.get(0).click();
-					}
-				}
+
+	}
+
+	public void clickShowAll()
+	{
+		int k=showAll.size();
+		if (k > 0) {
+			showAll.get(0).click();
+		}
+	}
 
 
 	/*-------------------------------------------------------------Test Case 391788:CWL_Verify records in "E/O" at Risk" filter folder------------------------------------------------------------*/
-				
-				
-				public void clickEoRisk()
-				{
-					clickShowAll();
-					EORisk.click();
-				}
-				
+
+
+	public void clickEoRisk()
+	{
+		clickShowAll();
+		EORisk.click();
+	}
+
 	/*----------------Test Case 391788:CWL_Verify records in "Future Follow Up" filter folder--------------------------*/			
 
-				public void clickFutureFollowUp()
-				{
-					clickShowAll();
-					FutureFollowUp.click();
-				}
+	public void clickFutureFollowUp()
+	{
+		clickShowAll();
+		FutureFollowUp.click();
+	}
 	/*----------------Test Case 422404:CWL_Verify records in "Zero Balance" filter folder--------------------------*/
-				public void clickZeroBalance()
-				{
-					clickShowAll();
-					zeroBalance.click();
-				}
+	public void clickZeroBalance()
+	{
+		clickShowAll();
+		zeroBalance.click();
+	}
 	/*---------------------------------------------------------------------Test Case 391809:CWL_Verify "Secondary Audit Action" functionality------------------------------------------------*/
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- R1Access/src/test/java/r1/steps/definitions/CWLStepDef.java
+++ R1Access/src/test/java/r1/steps/definitions/CWLStepDef.java
@@ -18,20 +18,20 @@ public class CWLStepDef extends BasePage {
 	
 	@When("^Click on \"([^\"]*)\" menu$")
 	public void click_on_menu(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.click_setting();
 	   
 	}
 
 	@Then("^List of sub-menus should be displayed in left panel$")
 	public void list_of_sub_menus_should_be_displayed_in_left_panel() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	     cwl.submenuList();;
 	}
 
 	@When("^Mouse hover on \"([^\"]*)\" menu$")
 	public void mouse_hover_on_menu(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.select_submenu("Payor and Plan Config");
 		
 	    
@@ -39,20 +39,20 @@ public class CWLStepDef extends BasePage {
 
 	@Then("^Sub menu list of worklist for \"([^\"]*)\" menu should be displayed$")
 	public void sub_menu_list_of_worklist_for_menu_should_be_displayed(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.settingSubmenuList();
 	}
 
 	@When("^Click on 'Conversion Plan Mapping' sub menu$")
 	public void click_on_Conversion_Plan_Mapping_sub_menu() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.payorcon_submenu("Conversion Plan Mapping");
 	    
 	}
 	
 	@Then("^All the necessary plan codes which are mapped into CWL should be displayed$")
 	public void all_the_necessary_plan_codes_which_are_mapped_into_CWL_should_be_displayed() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	    cwl.verifyCPMSize();
 	}
 	
@@ -61,20 +61,20 @@ public class CWLStepDef extends BasePage {
 	
 	@When("^user click on \"([^\"]*)\"$")
 	public void user_click_on(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	   cwl.clickOnpatientAccess();
 	}
 
 	@When("^user user clicks on Conversion Follow up$")
 	public void user_user_clicks_on_Conversion_Follow_up() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	  cwl.clickConversionfollowup();	}
 	
 
 
 	@Then("^verify following filter folder lists$")
 	public void verify_following_filter_folder_lists() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.filterList();
 	    
 	}
@@ -83,7 +83,7 @@ public class CWLStepDef extends BasePage {
 	
 	@When("^user clicks \"([^\"]*)\"$")
 	public void user_clicks(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.clickOnIsRisk();
 	    
 	}
@@ -91,27 +91,27 @@ public class CWLStepDef extends BasePage {
 	
 	@Then("^verify \"([^\"]*)\" value$")
 	public void verify_value(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.conversiontable("I", "S");
 	    
 	}
 
 	@Then("^Verify NFU date$")
 	public void verify_NFU_date() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.verifyNfuDt();
 	    
 	}
 	
 	@Then("^Verify Total Open Balance value$")
 	public void verify_Total_Open_Balance_value() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	   cwl.verifyTotalBal();
 	}
 
 	@Then("^LA column(.*) \"([^\"]*)\" / \"([^\"]*)\"$")
 	public void la_column(String arg1, String arg2, String arg3) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	    cwl.verifyLA();
 	}
 
@@ -119,13 +119,13 @@ public class CWLStepDef extends BasePage {
 	
 	@Then("^user clicks E/O at Risk$")
 	public void user_clicks_E_O_at_Risk() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.clickEoRisk();
 	}
 
 	@Then("^verify \"([^\"]*)\" value as 'E' OR 'O'$")
 	public void verify_value_as_E_OR_O(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.conversiontable("E", "O");
 	    
 	}
@@ -134,7 +134,7 @@ public class CWLStepDef extends BasePage {
 
 	@Then("^user clicks Future Follow Up$")
 	public void user_clicks_Future_Follow_Up() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.clickFutureFollowUp();
 	    
 	}
@@ -144,7 +144,7 @@ public class CWLStepDef extends BasePage {
 	
 	@Then("^user clicks Zero Balance$")
 	public void user_clicks_Zero_Balance() {
-	    // Write code here that turns the phrase above into concrete actions
+	    
 		cwl.clickZeroBalance();
 	    
 	}
@@ -152,77 +152,10 @@ public class CWLStepDef extends BasePage {
 
  @Then("^verify total open balance=(\\d+)$")
  public void verify_total_open_balance(int arg1) {
-    // Write code here that turns the phrase above into concrete actions
+    
 	 cwl.verifyTotalzeroBal();
    
   }
-	/*------------------------------------------Test Case 391809:CWL_Verify "Secondary Audit Action" functionality-------------------------------*/
- 
- @When("^select any records from conversion followup$")
- public void select_any_records_from_conversion_followup() {
-     // Write code here that turns the phrase above into concrete actions
-     
- }
-
- @Then("^record with \"([^\"]*)\" tab loaded should be opened up$")
- public void record_with_tab_loaded_should_be_opened_up(String arg1) {
-     // Write code here that turns the phrase above into concrete actions
-     
- }
-
- @Then("^verify Secondary Audit Action panel displayed$")
- public void verify_Secondary_Audit_Action_panel_displayed() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @When("^save secondary audit action without selecting checkboxes$")
- public void save_secondary_audit_action_without_selecting_checkboxes() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^Please select the action to update should be displayed$")
- public void please_select_the_action_to_update_should_be_displayed() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^No record should get added to the \"([^\"]*)\" panel$")
- public void no_record_should_get_added_to_the_panel(String arg1) {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @When("^check all the checkboxes and select value from Final status dropdown and save$")
- public void check_all_the_checkboxes_and_select_value_from_Final_status_dropdown_and_save() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^verify \"([^\"]*)\" panel records should be added$")
- public void verify_panel_records_should_be_added(String arg1) {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^If \"([^\"]*)\" is\"([^\"]*)\" no log goes for it$")
- public void if_is_no_log_goes_for_it(String arg1, String arg2) {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^Verify value in Disposition according to final status$")
- public void verify_value_in_Disposition_according_to_final_status() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
-
- @Then("^Verify mass-update panel$")
- public void verify_mass_update_panel() {
-     // Write code here that turns the phrase above into concrete actions
-    
- }
 
  
  
