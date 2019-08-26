1719d36bac59075006e1ac4b9acffb8bae760bcc
IN302616 <IN302616@NDR1-D85QF622A.india.accretivehealth.local>
2019-08-26 15:05:11 +0530

    change setting method

--- R1Access/src/test/java/r1/pages/Service2Page.java
+++ R1Access/src/test/java/r1/pages/Service2Page.java
@@ -129,7 +129,7 @@ public class Service2Page extends BasePage {
 	public void verifyServiceandResidualSettings(String facility, String serviceSetting, String residualSetting)
 			throws ClassNotFoundException, IOException, SQLException {
 		settingValue = Integer.parseInt(retrieveSettingValue(serviceSetting, facility));
-        System.out.print(settingValue);
+		
 		if (settingValue != 2) {
 			r1AccessCommonMethod.clickFooterR1AccesModulesTab("Settings");
 			r1AccessCommonMethod.clickSubSideR1HubModulesMenuLink("IT Tools");
@@ -146,9 +146,14 @@ public class Service2Page extends BasePage {
 			r1AccessCommonMethod.setFacilitySettingVal(facility, residualSetting, "2");
 		}
 		
+		if (settingValue != 2)
+		{
 		r1AccessCommonMethod.clickFooterR1AccesModulesTab("Settings");
 		r1AccessCommonMethod.clickSubSideR1HubModulesMenuLink("IT Tools");
 		r1AccessCommonMethod.clearCache();
+		r1AccessCommonMethod.clickFooterR1AccesModulesTab("Patient Access");
+		r1AccessCommonMethod.clickSubSideR1HubModulesMenuLink("Pre-Registration");		
+		}
 	}
 
 	public String retrieveSettingValue(String settingName,String facility) throws ClassNotFoundException, IOException, SQLException {
